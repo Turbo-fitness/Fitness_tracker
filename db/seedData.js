67c67bad65cdb23dd7a44f0d823a08a74c7b566d
Natalia Kielian <kieliannatalia1113@gmail.com>
2023-05-01 12:25:08 -0500

    creat/drop tables

--- db/seedData.js
+++ db/seedData.js
@@ -4,12 +4,58 @@ const client = require("./client")
 
 async function dropTables() {
   console.log("Dropping All Tables...")
-  // drop all tables, in the correct order
+  try {
+    console.log("Starting to drop tables");
+
+    await client.query(`
+    DROP TABLE IF EXISTS routine_activities;
+    DROP TABLE IF EXISTS activities;
+    DROP TABLE IF EXISTS routines;
+    DROP TABLE IF EXISTS users;
+    `);
+
+    console.log("Finished dropping tables!");
+  } catch (error) {
+      console.error("Error dropping tables!");
+    throw error; 
+  }
 }
 
 async function createTables() {
   console.log("Starting to build tables...")
-  // create all tables, in the correct order
+  try {
+    await client.query(`
+    CREATE TABLE users (
+      id SERIAL PRIMARY KEY,
+      username varchar(255) UNIQUE NOT NULL,
+      password varchar(255) NOT NULL
+    );
+        
+    CREATE TABLE routines (
+      id SERIAL PRIMARY KEY,
+      "creatorId" INTEGER REFERENCES users(id) NOT NULL, 
+      "isPublic" BOOLEAN DEFAULT TRUE,
+      name varchar(255) NOT NULL,
+      goal TEXT NOT NULL
+    );
+    
+    CREATE TABLE activities (
+      id SERIAL PRIMARY KEY,
+      name varchar(255) NOT NULL,
+      description TEXT NOT NULL
+    );
+
+    CREATE TABLE routine_activities (
+      id SERIAL PRIMARY KEY,
+      "routineId" INTEGER REFERENCES routines(id) NOT NULL, 
+      "activityId" INTEGER REFERENCES activities(id),
+      UNIQUE ("routineId", "activityId")
+    );
+    `)
+  } catch (error) {
+    console.error("Error building tables!");
+    throw error; 
+  }
 }
 
 /* 
