03871490f8e541999e9427bf76b68b20171517ef
Peter Laughlin <pwlaughlin@gmail.com>
2023-05-03 07:09:31 -0500

    pre-getALLroutines

--- db/routines.js
+++ db/routines.js
@@ -1,12 +1,40 @@
 const client = require("./client");
 
-async function createRoutine({ creatorId, isPublic, name, goal }) {}
-
+async function createRoutine({ creatorId, isPublic, name, goal }) {
+try{
+  const { rows: [routine]} = await client.query(`
+  
+  INSERT INTO routines ("creatorId", "isPublic", "name", "goal")
+  VALUES($1, $2, $3, $4)
+  RETURNING *;
+  `, [creatorId, isPublic, name, goal]
+  );
+console.log("this is my routines", routine)
+  return routine;
+  }catch (error){
+    console.error("could not create routine");
+  }
+}
 async function getRoutineById(id) {}
 
 async function getRoutinesWithoutActivities() {}
 
-async function getAllRoutines() {}
+async function getAllRoutines() {
+  try{
+    const { rows: routines } = await client.query(`
+    
+    SELECT routines.*, users.username AS "createorName" 
+    FROM routines
+    
+    `
+    );
+  
+    return routines;
+    }catch (error){
+      throw error;
+    }
+  }
+
 
 async function getAllPublicRoutines() {}
 
