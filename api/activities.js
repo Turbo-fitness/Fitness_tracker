6f7c5525efa18a8a870512f98af9db5452894da8
Peter Laughlin <pwlaughlin@gmail.com>
2023-05-14 07:54:10 -0500

    beh

--- api/activities.js
+++ api/activities.js
@@ -1,12 +1,53 @@
 const express = require('express');
-const router = express.Router();
-
+const activitiesRouter = express.Router();
+const { getAllActivities, createActivity} = require("../db");
 // GET /api/activities/:activityId/routines
 
 // GET /api/activities
-
+activitiesRouter.get('/', async (req, res, next) => {
+    try {
+      const activities = await getAllActivities();
+      res.json(activities);
+    } catch (error) {
+      next(error);
+    }
+  });
 // POST /api/activities
+
+activitiesRouter.post('/', async (req, res, next) => {
+  const { id, name, description } = req.body;
+
+  if (!id || !name || !description) {
+    return next({
+      name: 'InvalidInputError',
+      message: 'Missing required fields'
+    });
+  }
+
+  const postData = {
+    id: id,
+    name: name,
+    description: description,
+    authorId: req.user.id
+  };
+console.log(postData)
+  try {
+    const activity = await createActivity(postData);
+
+    if (activity) {
+      res.send({ activity });
+    } else {
+      return next({
+        name: 'CreateActivityError',
+        message: 'Unable to create Activity'
+      });
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
 
 // PATCH /api/activities/:activityId
 
-module.exports = router;
+module.exports = activitiesRouter;
